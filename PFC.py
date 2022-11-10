7ee24942cefd0654bef8320835772560519f2be5
Rémi Salles <remisalles.12@gmail.com>
2022-11-10 16:56:03 +0100

    fin pfc

--- PFC.py
+++ PFC.py
@@ -43,10 +43,10 @@
 #           afficher choix du bot est ciseau
 #   retourner choixBot
 
-#Définir la fonction option avec comme paramètre nombreDeManche, relancerUnePartie:
-#   dd
-
-
+#Définir la fonction option avec comme paramètre nombreDeManche:
+#   Afficher "Combien de Manche shouaites tu jouer ?"
+#   Assigner nombreDeManche avec le retour de l'execution de la fonction Input
+#   Retourner nombreDeManche
 
 
 #Définir la fonction game avec comme paramètre (choixBot,ChoixJoueur):
@@ -54,10 +54,16 @@
 #   On assigne à choixJoueur le retour de l'execution de Joueur
 #   On initialise la variable pointJoueur à 0
 #   On initialise la variable pointBot à 0
-#   Tant que pointJoueur est plus petit que 3 et pointBot est plus petit que 3  :
+#   Assigner relancerUnePartie avec True
+#   On appelle et execute la fonction option
+#   Tant que pointJoueur est plus petit que nombreDeManche et pointBot est plus petit que nombreDeManche et relancerUnePartie est égale à True :
 #       Alors
 #       On appelle et execute la fonction bot
-#       On appelle et execute la fonction Joueur 
+#       On appelle et execute la fonction Joueur
+
+#       Si pointBot ou PointJoueur = nombreDeManche:
+#           Alors assigner False à la variable relancerUnePartie
+
 #       Si choixJoueur et choixBot sont égaux alors : 
 #           On affiche "Egalité"
 #
@@ -81,6 +87,9 @@
 #       Si choixJoueur est égale à 3 et choixBot est égale à 1 :
 #           On affiche "Le bot à Gagné"
 #           Ajoute 1 à la variable pointBot 
+
+#   Afficher "Tu veux rejouer ?"
+#   On assigne à la variable relancerUnePartie le retour de l'execution de Input (true ou false)
 
 # FIN
 
